e620e45cba4990b9fa0cf2328c4729a099c547ad
Christian Colson <christiancolson91@gmail.com>
2021-10-20 13:33:11 -0500

    functions2lecture

---
+++ function2Lecture.js
@@ -0,0 +1,103 @@
+// const add = (num1, num2) => num1 + num2
+
+// const subtract = (num1, num2) => num1 - num2
+
+// const multiply = (num1, num2) => num1 * num2
+
+// const divide = ( num1, num2) => num1 / num2
+
+// const calculator = (num1, num2, callback) => {
+//   if (+num1 && +num2){
+//     num1 = +num1
+//     num2 = +num2
+//     return callback(num1, num2)
+//   } else {
+//     console.log('you need to send in numbers as the parameters')
+//   }
+// }
+// const result = calculator(1, 2, add)
+// console.log(result)
+
+const dogProducts = [
+  {
+    name: 'leash',
+    colors: ['red', 'blue', 'green'],
+    category: 1,
+    inventory: 30,
+    basePrice: 13.99, 
+    displayPrice: 13.99
+  }, 
+  {
+    name: 'chew toy',
+    colors: ['brown'],
+    category: 2,
+    inventory: 120,
+    basePrice: 6.00, 
+    displayPrice: 6.00
+  }, 
+  {
+    name: 'rope',
+    colors: ['blue & green', 'red & yellow'],
+    category: 2,
+    inventory: 75,
+    basePrice: 4.99, 
+    displayPrice: 4.99
+  }
+]
+
+const catProducts = [
+{
+  name: 'mouse toy', 
+  colors: ['pink', 'grey', 'black'], 
+  category: 2, 
+  inventory: 125, 
+  basePrice: 2.50, 
+  displayPrice: 2.50
+},
+{
+  name: 'cat sweater',
+  colors: ['black'],
+  category: 1,
+  inventory: 15,
+  basePrice: 10.00, 
+  displayPrice: 10.00
+}, 
+{
+  name: 'straching post',
+  colors: ['tan'],
+  category: 2,
+  inventory: 40,
+  basePrice: 22.99, 
+  displayPrice: 22.99
+}
+]
+
+const applyPercentDiscount = (product, discount) => {
+  product.displayPrice = product.basePrice * (1 - discount)
+}
+
+const applyFlatRateDiscount = (product, discount) => {
+  product.displayPrice = product.basePrice - discount
+}
+
+// const applyDiscounts = (arr, callback, discount) => {
+//   arr.forEach(product => {
+//     callback(product, discount)
+//   })
+// }
+
+// applyDiscounts(dogProducts, applyPercentDiscount, 0.25)
+
+// 
+const applyDiscountsByCategory = (arr, category, callback, discount) => {
+  arr.forEach(product => {
+    if (product.category === category){
+      callback(product, discount)
+    } else {
+      console.log('this product is not in the discounted category')
+    }
+  })
+}
+
+applyDiscountsByCategory(catProducts, 1, applyFlatRateDiscount, 1.00)
+console.log(catProducts)
